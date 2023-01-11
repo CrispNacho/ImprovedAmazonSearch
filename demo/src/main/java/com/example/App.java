5ee6ee2ef8d303a133fa04d30d4f70f0baaac99a
Evan Chadwick <evanchadwick@hotmail.com>
2023-01-11 14:47:39 -0500

    added shell pages

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,14 +1,16 @@
 package com.example;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
-
+import javafx.scene.control.ScrollBar;  
 import java.io.IOException;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Button;
 
 /**
  * JavaFX App
@@ -16,10 +18,17 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-    GridPane root = new GridPane();
+    ScrollPane otherPane = new ScrollPane();
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(root, 640, 480);
+        Button nextScreenBtn = new Button("Next Screen Button");
+        nextScreenBtn.setText("Next");
+
+        stage.setTitle("Amazon Improved Search");
+        Menu mainMenuScreen = new Menu();
+        GridPane mainMenuScreen1 = mainMenuScreen.implementScreen();
+        scene = new Scene(mainMenuScreen1, 640, 480);
+        mainMenuScreen1.addRow(0, nextScreenBtn);
         stage.setScene(scene);
         stage.show();
     }
---
+++ demo/src/main/java/com/example/Menu.java
@@ -0,0 +1,9 @@
+package com.example;
+import javafx.scene.layout.GridPane;
+public class Menu {
+    public GridPane implementScreen(){
+        GridPane screen = new GridPane();
+        
+        return screen;
+    }
+}
