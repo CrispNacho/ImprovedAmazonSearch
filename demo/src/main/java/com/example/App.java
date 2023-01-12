65d6acd91af735f17edb92013a8accfb6033bb5c
Evan Chadwick <evanchadwick@hotmail.com>
2023-01-12 09:41:34 -0500

    added basic buttons to iterate through screens

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -27,10 +27,22 @@ public class App extends Application {
         stage.setTitle("Amazon Improved Search");
 
         //Next buttons for respective screens
+
+        //Main Screen Buttons
         Button summaryScreenButton = new Button("Summary Dashboard");
         summaryScreenButton.setStyle("-fx-background-color: #9FE2BF ");
         Button searchScreenButton = new Button("Search");
         searchScreenButton.setStyle("-fx-background-color: #9FE2BF ");
+
+        //Prodcut Screen Buttons
+        Button summaryScreenButton1 = new Button("Summary Dashboard");
+        summaryScreenButton1.setStyle("-fx-background-color: #9FE2BF ");
+
+        //Summary Screen Buttons
+
+        Button searchScreenButton1= new Button("Search");
+        searchScreenButton1.setStyle("-fx-background-color: #9FE2BF ");
+
 
         
         //Create screens obects
@@ -39,26 +51,37 @@ public class App extends Application {
 
         ProductPage productScreen = new ProductPage();
         GridPane productScreen1 = productScreen.implementScreen();
-        System.out.println(productScreen.getProducts().get(0).name);
+        //System.out.println(productScreen.getProducts().get(0).name);
+        productScreen1.addRow(7, summaryScreenButton1);
 
         SummaryPage summaryPageScreen = new SummaryPage();
         GridPane summaryPageScreen1 = summaryPageScreen.implementScreen();
-
+        summaryPageScreen1.addRow(7, searchScreenButton1);
 
         //Creating scene objects
         Scene summaryScreenScene = new Scene(summaryPageScreen1,640, 480);
-
+        Scene productScreenScene = new Scene(productScreen1, 640, 480);
         
 
         
         //Next buttons event handlers
+
+        //Main Screen buttons
         summaryScreenButton.setOnAction(e -> {   
             stage.setScene(summaryScreenScene);
         }); 
         searchScreenButton.setOnAction(e -> {
-            scene = new Scene(productScreen1, 640, 480);
-            stage.setScene(scene);
+            stage.setScene(productScreenScene);
 
+        }); 
+
+        //Product Screen Buttons
+        summaryScreenButton1.setOnAction(e -> {   
+            stage.setScene(summaryScreenScene);
+        }); 
+        //Summary Screen Buttons
+        searchScreenButton1.setOnAction(e -> {   
+            stage.setScene(productScreenScene);
         }); 
 
         mainMenuScreen1.getChildren().addAll(searchScreenButton, summaryScreenButton);
